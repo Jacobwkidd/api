79d5fa5803b301f54e8832019e50a008c0246b4a
Jacob Kidd <kiddj1@students.westerntc.edu>
2023-10-24 14:50:44 -0500

    api

--- includes/models/Role.inc.php
+++ includes/models/Role.inc.php
@@ -1,3 +1,42 @@
 <?php 
 
 // Add your code here
+include("Model.inc.php");
+
+class Role extends Model{
+
+    public $id;
+    public $name;
+    public $description;
+    //constructor
+    function __construct($args=[]){
+        $this->id = $args["id"] ?? 0;
+        $this->name = $args["name"] ?? "";
+        $this->description = $args["description"] ?? "";
+    }
+
+    public function isValid(){
+        $valid = true;
+        $this->validationErrors = [];
+
+        //valid the id 
+        if(!is_numeric($this->id) || $this->id < 0){
+            $valid = false;
+            $this->validationErrors["id"] = "ID is not valid";
+        }
+
+        // name should be less than 30 characters
+        // name should not be empty
+        if(empty($this->name)){
+            $valid = false;
+            $this->validationErrors["name"] = "Name cannot be empty";
+        }
+        else if(strlen($this->name) > 30){
+            $valid = false;
+            $this->validationErrors["name"] = "Name cannot be longer than 30 characters";
+        }
+
+        return $valid;
+    }
+
+}
--- tests/RoleTests.php
+++ tests/RoleTests.php
@@ -1,5 +1,51 @@
 <?php
 
-// Add your code here
+include_once("../includes/models/Role.inc.php");
 
+// we need an associative array to pass into the constructor
+$options = [
+    "id" => 1,
+    "name" => "Test Role",
+    "description" => "This role is for testing"
+];
+
+// When each test completes, the result message will get added to this array:
+$testResults = [];
+
+// This function will run tests on the constructor in the Role class:
+testConstructor();
+
+// This function will run tests on the isValid() method:
+testIsValid();
+
+// This will echo the test results:
+echo(implode("<br>", $testResults));
+
+function testConstructor(){
+    global $testResults, $options;
+    $testResults[] = "<b>Testing Constructor</b>";
+
+    // Test 1 - Make sure we can instantiate a Role object
+    $r = new Role();
+    if($r){
+        $testResults[] = "PASS - created instance";
+    }else{
+        $testResults[] = "Fail - did not instance";
+    }
+    
+    // Test 2 - Make sure that the id property gets set properly
+    $r = new Role($options);
+    if($r->id === 1){
+        $testResults[] = "PASS - id set properly";
+    }else{
+        $testResults[] = "Fail - id NOT set properly";
+    }
+}
+
+function testIsValid(){
+    global $testResults, $options;
+    $testResults[] = "<b>Testing isValid()</b>";
+
+    
+}
 ?>
