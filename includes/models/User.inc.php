513ddf646b87c26e5b9911d4d7ad28173e36e75d
Jacob Kidd <kiddj1@students.westerntc.edu>
2023-10-31 14:43:12 -0500

    done

--- includes/models/User.inc.php
+++ includes/models/User.inc.php
@@ -60,9 +60,13 @@ class User extends Model{
 		// firstName must not be longer than 30 characters
 		// If the firstName is empty, you should add a 'firstName' key to $this->validationErrors with a value of "First Name is required"
 		// If the firstName is longer than 30 characters, you should add a 'firstName' key to $this->validationErrors with a value of "First Name must be 30 characters or less"
-		if(empty($this->firstName) || $this->firstName > 30){
+		if(empty($this->firstName)){
 			$valid = false;
-			$this->validationError["firstName"] = "first name is not valid";
+			$this->validationErrors["firstName"] = 'First Name is required';
+		}
+		else if(strlen($this->firstName) > 30){
+			$valid = false;
+			$this->validationErrors["firstName"] = "First Name must be 30 characters or less";
 		}
 
 
@@ -71,9 +75,13 @@ class User extends Model{
 		// lastName must not be longer than 30 characters
 		// If the lastName is empty, you should add a 'lastName' key to $this->validationErrors with a value of "Last Name is required"
 		// If the lastName is longer than 30 characters, you should add a 'lastName' key to $this->validationErrors with a value of "Last Name must be 30 characters or less"
-		if(empty($this->lastName) || $this->lastName > 30){
+		if(empty($this->lastName)){
 			$valid = false;
-			$this->validationError["firstName"] = "last name is not valid";
+			$this->validationErrors["lastName"] = "Last Name is required";
+		}
+		if(strlen($this->lastName) > 30){
+			$valid = false;
+			$this->validationErrors["lastName"] = "Last Name must be 30 characters or less";
 		}
 
 
@@ -84,13 +92,27 @@ class User extends Model{
 		// If email is empty, you should add an 'email' key to $this->validationErrors with a value of "Email is required"
 		// If email is not valid, you should add an 'email' key to $this->validationErrors with a value of "The email address is not valid"
 		// If email is longer than 100 characters, you should add an 'email' key to $this->validationErrors with a value of "Email must be 100 characters or less"
-		
+		if(empty($this->email)){
+			$valid = false;
+			$this->validationErrors["email"] = "Email is required";
+		}
+		else if(strlen($this->email) > 100){
+			$valid = false;
+			$this->validationErrors["email"] = "Email must be 100 characters or less";
+		}
+		else if(!filter_var($this->email, FILTER_VALIDATE_EMAIL)){
+			$valid = false;
+			$this->validationErrors["email"] = "The email address is not valid";
+		}
 
 
 		// validate roleId
 		// roleId should be a 1 or a 2 (it should be a number, not a string)
 		// Side Note: 1 = Standard User and 2 = Admin 
-		
+		if($this->roleId !== 1 || $this->roleId !== 2){
+			$valid = false;
+			$this->validationErrors["roleId"] = 'The role is not valid';
+		}
         
 
 		// validate password
@@ -98,6 +120,14 @@ class User extends Model{
 		// If the user id is 0 and the password is empty, then you should add a 'password' key 
 		// to $this->validationErrors with a value of "Password is required";
 		// Side NOTE: Ideally we'd be enforcing a strong password
+		if($this->id == 0){
+			$valid = false;
+			$this->validationErrors["password"] = "Password is required";
+			if(empty($this->password)){
+				$valid = false;
+				$this->validationErrors["password"] = "Password is required";
+			}
+		}
 		
 
 
@@ -107,7 +137,13 @@ class User extends Model{
 
 		// validate active, it must be either true or false
 		// SIDE NOTE: when a user is not active, they will not be able to log in
-		
+		if($this->active !== true || $this->active !== false){
+			$valid = false;
+			$this->validationErrors["active"] = 'The active setting is not valid';
+		}
+		else if($this->active !== true){
+			$valid = true;
+		}
 
 
 
